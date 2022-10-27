2925eeb0d3f30b4d1e929ef55b548be42e66052a
Joel Bonsi <bonsimaxi@gmail.com>
2022-10-26 21:34:38 -0300

    correccion del archivo JS

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -6,15 +6,18 @@ monto.innerHTML ="$" + 0;
 function mostrar(){
   const opcion = document.getElementById("inputCategoria").value; 
     console.log(opcion);
+    const cantidad = document.getElementById("inputCity").value;
     
+
     if(opcion == "estudiante"){
-        document.getElementById("monto").innerHTML = "$" + (200*0.8);
-    }
-     if(opcion== "trainee")document.getElementById("monto").innerHTML = "$" + (200*0.5);
-     
-     if(opcion=="junior"){
-        document.getElementById("monto").innerHTML = "$" + (200*0.15);
-     }
+        document.getElementById("monto").innerHTML = "$" + (parseInt(cantidad)*(200-(200*0.8)));
+    }else if(opcion== "trainee"){
+      document.getElementById("monto").innerHTML = "$" +(parseInt(cantidad)* (200-(200*0.5)));
+    }if(opcion=="junior"){
+        document.getElementById("monto").innerHTML = "$" + (parseInt(cantidad)*(200-(200*0.15)));
+     }else  if(opcion=="..."){
+      document.getElementById("monto").innerHTML = "$0";
+   }
 
     
     event.preventDefault();
